e7f603c455b3f9a39beb192b2c994c2351089696
giabao18 <phamgiabao1862001@gmail.com>
2021-10-04 20:15:34 +0700

    Ex3

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -12,10 +12,25 @@ Ex:
 #include <stdlib.h>
 #include <math.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int testcase = atoi(argv[1]);
 	//Your codes here
-	
+	for (int i = 2; i < testcase; i++)
+	{
+		int count = 0;
+		for (int j = 1; j <= sqrt(i); j++)
+		{
+			if (i % j == 0)
+			{
+				count++;
+			}
+		}
+		if (count == 1)
+		{
+			printf("%d ", i);
+		}
+	}
 	return 0;
 }
