d53dcde9bf1b8507d7211a29ec4a06ae84d5c165
giabao18 <phamgiabao1862001@gmail.com>
2021-10-04 21:24:19 +0700

    Ex4

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -19,11 +19,55 @@ Ex:
 #include <stdlib.h>
 #include <math.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int height = atoi(argv[1]);
 	int border = atoi(argv[2]);
 	//Your codes here
-	
-	return 0;
+	int count = 1;
+	for (int i = 1; i <= height; i++)
+	{
+		// in ky tu khoang trang
+		if (i <= border || i > (height - border))
+		{
+			for (int j = i; j < height; j++)
+			{
+				printf("   ");
+			}
+
+			// in ky tu ngoi sao
+			for (int j = 1; j <= (2 * i - 1); j++)
+			{
+				printf(" * ");
+			}
+
+			// xuong dong ke tiep
+			printf("\n");
+		}
+		else
+		{
+			for (int j = i; j < height; j++)
+			{
+				printf("   ");
+			}
+			// in ky tu ngoi sao
+			for (int j = 1; j <= border; j++)
+			{
+				printf(" * ");
+			}
+			for (int i = 0; i < count; i++)
+			{
+				printf("   ");
+			}
+			for (int j = 1; j <= border; j++)
+			{
+				printf(" * ");
+			}
+			// xuong dong ke tiep
+			count = count + 2;
+			printf("\n");
+		}
+		return 0;
+	}
 }
